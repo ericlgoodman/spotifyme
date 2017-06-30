5ea010aac8508ae80a612fa5b6f6388f1c256128
Eric Goodman <ericgoodman37@gmail.com>
2017-06-30 19:28:15 +0300

    Basic data pipe set up

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-// Imports
+/* Author: Eric Goodman */
+
+
+/* Imports */
 var express = require('express');
 var request = require('request');
+var rp = require('request-promise');
 var querystring = require('querystring');
 var cors = require('cors');
+var bluebird = require('bluebird');
 
 // For local development use config file
 try {
     var config = require('./config.json');
 }
-catch(e) {
+catch (e) {
     if (!(e instanceof Error && e.code === "MODULE_NOT_FOUND")) {
         throw e;
     }
@@ -23,17 +28,48 @@ var REFRESH_TOKEN = process.env.REFRESH_TOKEN || config.REFRESH_TOKEN;
 var app = express();
 app.use(cors());
 
-app.get('/artists/time_range/:time_range', function (req, res) {
-    var time_range = req.params.time_range,
-        acceptedTimeRanges = ['medium_term', 'long_term'];
+/**
+ * Make get request from Spotify API
+ * @param url - Endpoint API url
+ * @param accessToken - Spotify access token
+ * @returns {Promise} - JSON
+ */
+function getFromSpotify(url, accessToken) {
+    // Set request parameters
+    return new Promise(function (resolve, reject) {
+        var options = {
+            url: url,
+            headers: {'Authorization': 'Bearer ' + accessToken},
+            json: true
+        };
 
-    if (!acceptedTimeRanges.includes(time_range)) {
-        res.status(400).send('Bad request');
+        request.get(options, function (error, response, body) {
+            if (!error) {
+                resolve(body);
+            }
+            else {
+                console.error("Error retrieving data from Spotify: ", error);
+                reject(error);
+            }
+        });
+    });
+}
+
+/**
+ * Our API endpoint
+ */
+app.get('/', function (req, res) {
+
+    // Validate
+    validSources = ['ericgoodman.me', 'localhost'];
+    if (!validSources.includes(req.hostname)) {
+        res.status(400).send('Invalid request');
     }
 
     // Refresh token
-    var authCredentials = {
-        url: 'https://accounts.spotify.com/api/token',
+    var authOptions = {
+        method: 'POST',
+        uri: 'https://accounts.spotify.com/api/token',
         headers: {'Authorization': 'Basic ' + (new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))},
         form: {
             grant_type: 'refresh_token',
@@ -42,17 +78,37 @@ app.get('/artists/time_range/:time_range', function (req, res) {
         json: true
     };
 
-    request.post(authCredentials, function (error, response, body) {
-        if (error) {
-            res.status(500).send({'Error: ' : error})
-        }
-        if (!error && response.statusCode === 200) {
-            var access_token = body.access_token;
-            res.send({
-                'access_token': access_token
-            });
-        }
-    });
+    var spotifyReqests = {
+        followedArtistsUrl: 'https://api.spotify.com/v1/me/following',
+        recentlyPlayedUrl: 'https://api.spotify.com/v1/me/player/recently-played',
+        playlistsUrl: 'https://api.spotify.com/v1/me/playlists',
+        tracksUrl: 'https://api.spotify.com/v1/me/tracks'
+    };
+
+    rp(authOptions)
+        .then(function (body) {
+
+            // Obtain access token necessary for requests
+            var accessToken = body.access_token;
+
+            /*[String]*/
+            var requests = [];
+
+            for (var name in spotifyReqests) {
+                var url = spotifyReqests[name];
+                requests.push(getFromSpotify(url, accessToken));
+            }
+            // Asynchronously request all -> join after
+            return Promise.all(requests)
+        })
+        .then(function (data) {
+            res.send(data);
+        })
+        .catch(function (err) {
+            console.error('Error:', err);
+            res.status(500).send({'Error:': err.message});
+        });
+
 });
 
 /* Listen */
